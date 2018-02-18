939feb4f9070fbc819bad3e231c1377a6ffac08f
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-02-18 16:29:10 +0100

    implement test function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('test-for: index.js just ran')
+#!/usr/bin/env node
+
+'use strict'
+
+const spawn = require('child_process').spawn
+const cmd = require('./cmd')
+
+let count = 0
+const MAX = cmd.N
+
+function test () {
+  if (++count > MAX) {
+    process.exit(0)
+  }
+  const child = spawn('npm', [ 'test' ], { stdio: 'inherit' })
+  child.on('exit', code => {
+    if (code !== 0) {
+      console.log(`Test number ${count} broke!`)
+      process.exit(code)
+    } else {
+      test()
+    }
+  })
+}
+
+test()
