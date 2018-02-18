31a4840de4f4f68972f22f0ea86cd0ac9f9b3822
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-02-18 16:24:21 +0100

    -n is optional, defaults to Infinity

--- cmd.js
+++ cmd.js
@@ -2,7 +2,7 @@ const cmd = require('commander')
 
 cmd
   .version(require('./package.json').version, '-v, --version')
-  .option('-n <N>', 'abort after N runs (optional)', parseInt)
+  .option('-n [N]', 'abort after N runs', parseInt, +Infinity)
   .parse(process.argv)
 
 module.exports = cmd
