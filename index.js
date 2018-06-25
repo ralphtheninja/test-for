f0eabffa96ed4a45fcb46658c9042e2206431eb3
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-25 15:00:12 +0200

    Add some colored output

--- index.js
+++ index.js
@@ -3,24 +3,38 @@
 'use strict'
 
 const spawn = require('child_process').spawn
+const chalk = require('chalk')
 const cmd = require('./cmd')
 
 let count = 0
 const MAX = cmd.N
 
 function test () {
-  if (++count > MAX) {
+  if (count === MAX) {
+    console.log(chalk.yellow(`\nFinished running ${count} tests!`))
     process.exit(0)
   }
+  ++count
   const child = spawn('npm', [ 'test' ], { stdio: 'inherit' })
   child.on('exit', code => {
     if (code !== 0) {
-      console.log(`Test number ${count} broke!`)
+      console.log(chalk.red(`Test number ${count} broke!`))
       process.exit(code)
     } else {
+      if (MAX === +Infinity) {
+        console.log(chalk.green(`Test ${count} [OK]`))
+      } else {
+        console.log(chalk.green(`Test ${count}/${MAX} [OK]`))
+      }
       test()
     }
   })
+}
+
+if (MAX === +Infinity) {
+  console.log(chalk.yellow('Running until it breaks!'))
+} else {
+  console.log(chalk.yellow(`Running max ${MAX} tests or until it breaks!`))
 }
 
 test()
